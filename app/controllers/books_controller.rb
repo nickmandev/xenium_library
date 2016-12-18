4ea7729605ad65e12af5250874fccb3f09a41305
Nick <nickmandev@gmail.com>
2016-12-18 18:56:28 +0200

    Working on design

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,18 @@ class BooksController < ApplicationController
     @reviews = Review.where(:book_id => params[:id])
   end
 
+  def search
+    results = []
+    search = params[:name].to_s.downcase
+    @books.each do |book|
+     if book.title.downcase.include?(search)
+       results.push(book)
+     end
+    end
+    results.to_json
+    results
+  end
+
   private
   def book_params
     params.require(:book).permit(:title,:description,:isbn, picture_attributes: [:picture])
